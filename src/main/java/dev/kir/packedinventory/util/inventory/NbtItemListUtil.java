6c5f6ab3d83e98bd157287a9a0087ee48222cf24
Kir_Antipov <kp.antipov@gmail.com>
2022-07-19 11:44:09 +0300

    Implemented utility class to handle Nbt item lists

---
+++ src/main/java/dev/kir/packedinventory/util/inventory/NbtItemListUtil.java
@@ -0,0 +1,103 @@
+package dev.kir.packedinventory.util.inventory;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.nbt.NbtList;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+
+public final class NbtItemListUtil {
+    private static final Comparator<NbtElement> COMPARATOR = Comparator.comparingInt(x -> x instanceof NbtCompound ? ((NbtCompound)x).getByte(InventoryUtil.SLOT_KEY) : 0);
+
+    @SuppressWarnings("unchecked")
+    public static void clean(NbtList list) {
+        Iterator<NbtCompound> iterator = (Iterator<NbtCompound>)(Object)list.iterator();
+        while (iterator.hasNext()) {
+            NbtCompound nbt = iterator.next();
+            String id = nbt.getString("id");
+            if ("air".equals(id) || "minecraft:air".equals(id) || nbt.getByte(InventoryUtil.COUNT_KEY) <= 0) {
+                iterator.remove();
+            }
+        }
+    }
+
+    public static void sort(NbtList list) {
+        list.sort(COMPARATOR);
+    }
+
+    public static int binarySearch(NbtList list, int slot) {
+        NbtCompound target = new NbtCompound();
+        target.putInt(InventoryUtil.SLOT_KEY, slot);
+        return Collections.binarySearch(list, target, COMPARATOR);
+    }
+
+    public static ItemStack get(NbtList list, int slot) {
+        int i = binarySearch(list, slot);
+        return i < 0 ? ItemStack.EMPTY : asItemStack(list.getCompound(i));
+    }
+
+    public static ItemStack remove(NbtList list, int slot) {
+        int i = binarySearch(list, slot);
+        if (i < 0) {
+            return ItemStack.EMPTY;
+        }
+
+        ItemStack stack = asItemStack(list.getCompound(i));
+        list.remove(i);
+        return stack;
+    }
+
+    public static ItemStack remove(NbtList list, int slot, int amount) {
+        int i = binarySearch(list, slot);
+        if (i < 0) {
+            return ItemStack.EMPTY;
+        }
+
+        NbtCompound stackNbt = list.getCompound(i);
+        ItemStack stack = asItemStack(stackNbt);
+        ItemStack splitStack = stack.split(amount);
+        if (stack.isEmpty()) {
+            list.remove(i);
+        } else {
+            stackNbt.putByte(InventoryUtil.COUNT_KEY, (byte)stack.getCount());
+        }
+        return splitStack;
+    }
+
+    public static void insert(NbtList list, int slot, ItemStack stack) {
+        if (stack.isEmpty()) {
+            NbtItemListUtil.remove(list, slot);
+            return;
+        }
+
+        NbtCompound stackNbt = asCompound(stack, slot);
+        int i = binarySearch(list, slot);
+        if (i >= 0) {
+            list.set(i, stackNbt);
+            return;
+        }
+
+        i = -i - 1;
+        if (i >= list.size()) {
+            list.add(stackNbt);
+        } else {
+            list.add(i, stackNbt);
+        }
+    }
+
+    private static NbtCompound asCompound(ItemStack stack, int slot) {
+        NbtCompound nbt = new NbtCompound();
+        nbt.putByte(InventoryUtil.SLOT_KEY, (byte)slot);
+        stack.writeNbt(nbt);
+        return nbt;
+    }
+
+    private static ItemStack asItemStack(NbtCompound nbt) {
+        return ItemStack.fromNbt(nbt);
+    }
+
+    private NbtItemListUtil() { }
+}
