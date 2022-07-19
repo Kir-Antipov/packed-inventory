4440439a892189b2b58d364d5fc7fa70b0c34aee
Kir_Antipov <kp.antipov@gmail.com>
2022-07-19 12:13:44 +0300

    Made mod config interface

---
+++ src/main/java/dev/kir/packedinventory/config/PackedInventoryConfig.java
@@ -0,0 +1,8 @@
+package dev.kir.packedinventory.config;
+
+import dev.kir.packedinventory.api.v1.config.PackedInventoryApiConfig;
+import dev.kir.packedinventory.compat.cloth.PackedInventoryClothConfig;
+import net.fabricmc.loader.api.FabricLoader;
+
+public interface PackedInventoryConfig extends PackedInventoryApiConfig {
+}
