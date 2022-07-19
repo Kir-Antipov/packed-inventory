ca14c44d2be63e9bca4e9448906d208a01d8225f
Kir_Antipov <kp.antipov@gmail.com>
2022-07-19 12:35:05 +0300

    Registered mod packets

---
+++ src/main/java/dev/kir/packedinventory/networking/PackedInventoryPackets.java
@@ -0,0 +1,22 @@
+package dev.kir.packedinventory.networking;
+
+import dev.kir.packedinventory.api.v1.networking.PackedInventoryEditRequest;
+import dev.kir.packedinventory.api.v1.networking.TooltipSyncRequest;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+
+public final class PackedInventoryPackets {
+    public static void init() {
+        TooltipSyncRequest.registerServerReceiver(ServerPlayNetworking::registerGlobalReceiver);
+        PackedInventoryEditRequest.registerServerReceiver(ServerPlayNetworking::registerGlobalReceiver);
+    }
+
+    @Environment(EnvType.CLIENT)
+    public static void initClient() {
+        TooltipSyncRequest.registerClientReceiver(ClientPlayNetworking::registerGlobalReceiver);
+    }
+
+    private PackedInventoryPackets() { }
+}
