16f63e1ab94825c192692574df79b835e86c75f7
Kir_Antipov <kp.antipov@gmail.com>
2023-12-27 16:42:04 +0300

    Forced the cursor to preserve its position

--- src/main/java/dev/kir/packedinventory/mixin/MouseMixin.java
+++ src/main/java/dev/kir/packedinventory/mixin/MouseMixin.java
@@ -1,5 +1,7 @@
 package dev.kir.packedinventory.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.kir.packedinventory.client.input.KeyInfo;
 import dev.kir.packedinventory.client.screen.CustomHandleableScreen;
 import net.fabricmc.api.EnvType;
@@ -7,6 +9,8 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.util.GlfwUtil;
+import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,7 +20,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
-@Mixin(Mouse.class)
+@Mixin(value = Mouse.class, priority = 10000)
 abstract class MouseMixin {
     @Shadow
     private @Final MinecraftClient client;
@@ -26,6 +30,12 @@ abstract class MouseMixin {
 
     @Shadow
     private double y;
+
+    private float lastMouseLockTime;
+
+    private float lastX;
+
+    private float lastY;
 
     @Inject(method = "onMouseButton", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;wrapScreenError(Ljava/lang/Runnable;Ljava/lang/String;Ljava/lang/String;)V"))
     private void onMouseButton(long window, int button, int action, int mods, CallbackInfo ci) {
@@ -66,5 +76,37 @@ abstract class MouseMixin {
             "customMouseMoved event handler",
             screen.getClass().getName()
         );
+    }
+
+    @Inject(method = "lockCursor", at = @At(value = "FIELD", target = "Lnet/minecraft/client/Mouse;x:D", ordinal = 0, shift = At.Shift.BEFORE))
+    private void lockCursor(CallbackInfo ci) {
+        this.lastX = (float)this.x;
+        this.lastY = (float)this.y;
+        this.lastMouseLockTime = (float)GlfwUtil.getTime();
+    }
+
+    @WrapOperation(method = "unlockCursor", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/InputUtil;setCursorParameters(JIDD)V", ordinal = 0))
+    private void unlockCursor(long window, int cursorState, double x, double y, Operation<Void> setCursor) {
+        final float MAX_SECONDS_SINCE_LAST_LOCK = 0.05F;
+
+        float currentTime = (float)GlfwUtil.getTime();
+        float secondsSinceLastLock = currentTime - this.lastMouseLockTime;
+
+        if (secondsSinceLastLock <= MAX_SECONDS_SINCE_LAST_LOCK) {
+            this.x = x = this.lastX;
+            this.y = y = this.lastY;
+
+            // If we are here, it means that the `lockCursor` method has been called
+            // just before this one.
+            // `lockCursor` sets the cursor position to the center of the screen using the
+            // `GLFW_CURSOR_DISABLED` flag. For some reason, if we set the cursor position
+            // to some other place right after that using the `GLFW_CURSOR_NORMAL` flag,
+            // new coordinates will be simply ignored. However, changing the cursor state
+            // back and forth a few times fixes the problem. So, let's just do that.
+            InputUtil.setCursorParameters(window, InputUtil.GLFW_CURSOR_NORMAL, x, y);
+            InputUtil.setCursorParameters(window, InputUtil.GLFW_CURSOR_DISABLED, x, y);
+        }
+
+        setCursor.call(window, cursorState, x, y);
     }
 }
