28298408e5a75a0b7c7a0db3d6d767090b201e80
Kir_Antipov <kp.antipov@gmail.com>
2023-12-29 10:47:12 +0000

    Registered `InventoryActionRequest`

--- src/main/java/dev/kir/packedinventory/networking/PackedInventoryPackets.java
+++ src/main/java/dev/kir/packedinventory/networking/PackedInventoryPackets.java
@@ -1,5 +1,6 @@
 package dev.kir.packedinventory.networking;
 
+import dev.kir.packedinventory.api.v1.networking.InventoryActionRequest;
 import dev.kir.packedinventory.api.v1.networking.PackedInventoryBulkEditRequest;
 import dev.kir.packedinventory.api.v1.networking.PackedInventoryEditRequest;
 import dev.kir.packedinventory.api.v1.networking.TooltipSyncRequest;
@@ -11,6 +12,7 @@ import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 public final class PackedInventoryPackets {
     public static void init() {
         TooltipSyncRequest.registerServerReceiver(ServerPlayNetworking::registerGlobalReceiver);
+        InventoryActionRequest.registerServerReceiver(ServerPlayNetworking::registerGlobalReceiver);
         PackedInventoryEditRequest.registerServerReceiver(ServerPlayNetworking::registerGlobalReceiver);
         PackedInventoryBulkEditRequest.registerServerReceiver(ServerPlayNetworking::registerGlobalReceiver);
     }
